9720aa8f3dd761497b8ee2fd29f15974bd903a9c
Deepak yadav 202078 <90127239+Deepakyadav202078@users.noreply.github.com>
2022-05-07 11:17:45 +0530

    Create lab10c.cpp

---
+++ lab10c.cpp
@@ -0,0 +1,39 @@
+//Concept of paramertrized constructor
+
+#include <iostream>
+using namespace std;
+
+class Math
+{
+    private:
+    int x,y,z ;
+    public: 
+    Math(int x1, int y1, int z1)
+    {
+        x=x1;
+        y=y1;
+        z=z1;
+    }
+    int getX()
+    {
+        return x;
+    }
+    int getY()
+    {
+        return y;
+    }
+    int getZ()
+    {
+        return z;
+    }
+};
+int main()
+{
+   
+    Math m(10, 11, 12);
+    cout<< "The value of x is : "<<m.getX()<<endl
+        << "The value of y is : "<<m.getY()<<endl
+        << "The value of z is : "<<m.getZ();
+        
+    return 0;
+}
