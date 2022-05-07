30b5e5751a0c7c33b8cd84505e3004a05f03ec59
Deepak yadav 202078 <90127239+Deepakyadav202078@users.noreply.github.com>
2022-05-07 11:11:00 +0530

    Create lab10b.cpp

---
+++ lab10b.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Constructor
+{
+    public:
+    int a,b;
+   Constructor()
+    {
+        a=10;
+        b=20;
+    }
+   
+};
+
+int main()
+{
+    Constructor c;
+     cout << "The value of a is : "<<c.a<<endl
+          << "The value of b is : "<<c.b ;
+    return 0;
+}
